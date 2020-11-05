8397fc5b47ac8a502e5ced268b7f58067025ff0c
el10686 <vnapostolop@gmail.com>
2020-11-05 15:46:07 +0200

    Modify mr.creosote

--- mr.creosote.c
+++ mr.creosote.c
@@ -3,47 +3,47 @@
 
 int main()
 {
-    long int N, b, c, i, j;
-    int a;
+	long int N, b, c, i, j;
+	int a;
 
-    scanf("%li %i %li %li", &N, &a, &b, &c);
+	scanf("%li %i %li %li", &N, &a, &b, &c);
 
-    int *x;
+	int *x;
 
-    x = malloc(sizeof(int) * N);
+	x = malloc(sizeof(int) * N);
 
-    for(i = 0; i < N; i++)
-    {
-        scanf("%i", &x[i]);
-    }
+	for(i = 0; i < N; i++)
+	{
+		scanf("%i", &x[i]);
+	}
 
-    long long *partial_sums, *max, temp;
+	long long *partial_sums, *max, temp;
 
-    partial_sums = malloc(sizeof(long long int) * N);
-    max = malloc(sizeof(long long int) * N);
+	partial_sums = malloc(sizeof(long long int) * N);
+	max = malloc(sizeof(long long int) * N);
 
-    partial_sums[0] = x[0];
+	partial_sums[0] = x[0];
 
-    max[0] = a * x[0] * x[0] + b * x[0] + c;
+	max[0] = a * x[0] * x[0] + b * x[0] + c;
 
-    for(i = 1; i < N; i++)
-    {
-        partial_sums[i] = partial_sums[i - 1] + x[i];
+	for(i = 1; i < N; i++)
+	{
+		partial_sums[i] = partial_sums[i - 1] + x[i];
 
-        max[i] = a * partial_sums[i] * partial_sums[i] + b * partial_sums[i] + c;
+		max[i] = a * partial_sums[i] * partial_sums[i] + b * partial_sums[i] + c;
 
-        for(j = 1; j <= i; j++)
-        {
-            temp = max[j - 1] + a * (partial_sums[i] - partial_sums[j - 1]) * (partial_sums[i] - partial_sums[j - 1]) + b * (partial_sums[i] - partial_sums[j - 1]) + c;
+		for(j = 1; j <= i; j++)
+		{
+			temp = max[j - 1] + a * (partial_sums[i] - partial_sums[j - 1]) * (partial_sums[i] - partial_sums[j - 1]) + b * (partial_sums[i] - partial_sums[j - 1]) + c;
 
-            if(temp > max[i])
-            {
-                max[i] = temp;
-            }
-        }
-    }
+			if(temp > max[i])
+			{
+				max[i] = temp;
+			}
+		}
+	}
 
-    printf("%lli\n", max[N-1]);
+	printf("%lli\n", max[N-1]);
 
-    return 0;
+	return 0;
 }
